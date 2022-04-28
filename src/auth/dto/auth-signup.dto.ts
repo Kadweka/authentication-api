d320168204e40d7ae49981fbb72715203962f397
Kadweka <husseinkatana17@gmail.com>
2022-04-28 13:45:20 +0300

    adding validtors to our columns

--- src/auth/dto/auth-signup.dto.ts
+++ src/auth/dto/auth-signup.dto.ts
@@ -1,6 +1,18 @@
+import { IsEmail, IsString, Matches, MinLength } from "class-validator";
+
 export class AuthSignUpDto{
+
+    @IsEmail()
     email:string;
+    @IsString()
+    @MinLength(8)
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+        message:
+          'password must contain uppercase, lowercase, number and special character',
+      })
     password:string;
+    @IsString()
     firstName:string;
+    @IsString()
     lastName:string
 }
