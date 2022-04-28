060fe279b0a5a790bd93ec7f9ec76496f46bb2d1
Kadweka <husseinkatana17@gmail.com>
2022-04-28 13:36:53 +0300

    adding the user repository into our auth module and add missing imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
-
-@Module({})
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UserRepository } from './user.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+@Module({
+    imports:[TypeOrmModule.forFeature([UserRepository])],
+    controllers:[AuthController],
+    providers:[AuthService]
+})
 export class AuthModule {}
