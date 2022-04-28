343af10c300a2a04d272d56a6abfd48a7dd276cb
Kadweka <husseinkatana17@gmail.com>
2022-04-28 18:21:29 +0300

    doing it live

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {SwaggerModule,DocumentBuilder} from '@nestjs/swagger'
+
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+  .setTitle('Expense Management Center')
+  .setDescription('This is to help Manager your Funds')
+  .setVersion('1.0')
+  .addTag('Expense Manager')
+  .build();
+
+const document = SwaggerModule.createDocument(
+  app,
+  config
+  );
+
+SwaggerModule.setup('api', app, document);
+app.enableCors();
+await app.listen(process.env.PORT || 3000);
+  // await app.listen(3000);
+  if(module.hot){
+    module.hot.accept();
+    module.hot.dispose(()=>app.close());
+  }
 }
+
 bootstrap();
