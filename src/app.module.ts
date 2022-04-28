fb8a8dc01cee5ba8d5b90f77c4faea76f2718fcb
Kadweka <husseinkatana17@gmail.com>
2022-04-28 14:37:51 +0300

    changed the view

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthController } from './auth/auth.controller';
-import { AuthService } from './auth/auth.service';
-import { AuthModule } from './auth/auth.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from 'typeorm.config';
+
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), AuthModule],
-  controllers: [AppController, AuthController],
-  providers: [AppService,AuthModule, AuthService],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
