6f817606a03b966e0205a64678c4827a3f1ef1ec
Kadweka <husseinkatana17@gmail.com>
2022-04-28 13:59:42 +0300

    inject our service to be accesible

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthSignUpDto } from './dto/auth-signup.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(
+        private authService:AuthService
+    ){}
+    @Post('/register')
+    async signUp(@Body()AuthSignUpDto:AuthSignUpDto):Promise<string>{
+        return this.authService.signUp(AuthSignUpDto)
+    }
+}
