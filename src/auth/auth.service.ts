77f8fb2d9d3c9962056f2fc231e5ee4973e23cbe
Kadweka <husseinkatana17@gmail.com>
2022-04-28 13:55:44 +0300

    make signup method accessible whenenver we inject auth.service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthSignUpDto } from './dto/auth-signup.dto';
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(UserRepository)
         private userRepository:UserRepository
     ){}
+    async signUp(AuthSignUpDto:AuthSignUpDto):Promise<string>{
+        return this.userRepository.signUp(AuthSignUpDto)
+    }
 }
