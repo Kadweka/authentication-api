c6ca381abec1a5c62038e83b6968730a074eb385
Kadweka <husseinkatana17@gmail.com>
2022-04-29 09:39:52 +0300

    adding the expense module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
+import { ExpenseModule } from './expense/expense.module';
 
 @Module({
   // Updated imports to finailize configuration
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), AuthModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), AuthModule, ExpenseModule],
   controllers: [AppController],
   providers: [AppService],
 })
